5146c55abcd76abfc59ee146a5cb67aca46fd868
skanakia <skanakia1@gmail.com>
2018-06-07 22:52:59 -0400

    built myTweets function

--- liri.js
+++ liri.js
@@ -1,5 +1,6 @@
 var keys = require("./keys.js");
 var request = require("request");
+var fs = require("fs");
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
 
@@ -11,45 +12,62 @@ function movieThis() {
     var movieName = "";
 
     for (var i = 3; i < nodeArgs.length; i++) {
-    
-      if (i > 3 && i < nodeArgs.length) {
-    
-        movieName = movieName + "+" + nodeArgs[i];
-    
-      }
-    
-      else {
-    
-        movieName += nodeArgs[i];
-    
-      }
+
+        if (i > 3 && i < nodeArgs.length) {
+
+            movieName = movieName + "+" + nodeArgs[i];
+
+        }
+
+        else {
+
+            movieName += nodeArgs[i];
+
+        }
     }
-    
+
     var queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
-    
+
     console.log(queryUrl);
-    
-    request(queryUrl, function(error, response, body) {
-    
-      if (!error && response.statusCode === 200) {
-        console.log("Title: " + JSON.parse(body).Title);
-        console.log("Release Year: " + JSON.parse(body).Year);
-        console.log("Rating: " + JSON.parse(body).imdbRating);
-        console.log("Release Year: " + JSON.parse(body).tomatoRating);
-        console.log("Release Year: " + JSON.parse(body).Country);
-        console.log("Release Year: " + JSON.parse(body).Language);
-        console.log("Release Year: " + JSON.parse(body).Plot);
-        console.log("Release Year: " + JSON.parse(body).Actors);
-      }
+
+    request(queryUrl, function (error, response, body) {
+
+        if (!error && response.statusCode === 200) {
+            console.log("Title: " + JSON.parse(body).Title);
+            console.log("Release Year: " + JSON.parse(body).Year);
+            console.log("Rating: " + JSON.parse(body).imdbRating);
+            console.log("Release Year: " + JSON.parse(body).tomatoRating);
+            console.log("Release Year: " + JSON.parse(body).Country);
+            console.log("Release Year: " + JSON.parse(body).Language);
+            console.log("Release Year: " + JSON.parse(body).Plot);
+            console.log("Release Year: " + JSON.parse(body).Actors);
+        }
     });
-    
-    
+
+
 }
 
 
 function myTweets() {
-    
+    var tweetsArr = [];
+    var params = { screen_name: 'KanakiaSameer', count: 20, trim_user: true, exclude_replies: true };
+    client.get('statuses/user_timeline', params, function (error, tweets, response) {
+        if (!error && response.statusCode === 200) {
+            tweetsArr = tweets;
+
+            for (i = 0; i < tweetsArray.length; i++) {
+                console.log("Tweet: " + tweetsArray[i].text);
+                console.log("Time Stamp: " + tweetsArray[i].created_at)
+                console.log('-------------------');
+            }
+        }
+        else {
+            console.log(error);
+        }
+    });
+
 }
+
 
 
 
